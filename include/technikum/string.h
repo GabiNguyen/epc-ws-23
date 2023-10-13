3f33dcfad9fd09de64145f62fe650c22c158d85e
Nico Lerchl <nico@lerchl.one>
2023-10-13 10:02:07 +0200

    const char as iterator value type

--- include/technikum/string.h
+++ include/technikum/string.h
@@ -14,7 +14,7 @@ namespace technikum {
             struct iterator {
                 using iterator_category = std::bidirectional_iterator_tag;
                 using difference_type   = std::ptrdiff_t;
-                using value_type        = char;
+                using value_type        = const char;
                 using pointer           = value_type*;
                 using reference         = value_type&;
 
